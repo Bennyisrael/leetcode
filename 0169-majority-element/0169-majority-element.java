f55848e5dfaaee6c0c2bb907432320a99733ddea
Bennyisrael <112539918+Bennyisrael@users.noreply.github.com>
2023-12-21 19:24:03 +0530

    Time: 14 ms (18.92%), Space: 49.1 MB (13.33%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,11 +1,24 @@
-class Solution {
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {3, 3, 4};
+        int result = solution.majorityElement(nums);
+        System.out.println("Majority Element: " + result);
+    }
+
     public int majorityElement(int[] nums) {
         Map<Integer, Integer> h = new HashMap<>();
+
+        // Count occurrences of each element
         for (int num : nums) {
             h.put(num, h.getOrDefault(num, 0) + 1);
         }
+
+        // Create a list of map entries and sort it based on values (counts)
         List<Map.Entry<Integer, Integer>> sortedEntries = new ArrayList<>(h.entrySet());
-        Collections.sort(sortedEntries, Comparator.comparingInt(Map.Entry::getKey));
-        return sortedEntries.get(sortedEntries.size() / 2).getKey();
+        Collections.sort(sortedEntries, Comparator.comparingInt(Map.Entry::getValue));
+
+        // Return the majority element (last element in the sorted list)
+        return sortedEntries.get(sortedEntries.size() - 1).getKey();
     }
 }
