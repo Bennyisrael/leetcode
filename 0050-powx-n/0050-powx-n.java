a37f8bc866e5cfc7255e14c39be273aa6b50267d
Bennyisrael <112539918+Bennyisrael@users.noreply.github.com>
2024-02-21 19:08:34 +0530

    Time: 0 ms (100%), Space: 42 MB (64.4%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,22 @@
 class Solution {
     public double myPow(double x, int n) {
-        return (double)Math.pow(x,n);
+        double result = 1.0;
+        if (n == 0)
+            return 1.0;
+        if (n < 0) {
+            x = 1 / x;
+            if (n == Integer.MIN_VALUE) {
+                result *= x;
+                n++;
+            }
+            n = -n;
+        }
+        while (n > 0) {
+            if (n % 2 == 1)
+                result *= x;
+            x *= x;
+            n /= 2;
+        }
+        return result;
     }
 }
