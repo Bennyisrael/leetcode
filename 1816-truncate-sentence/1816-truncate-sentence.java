98d04cab170949325c6eacd0a9e14833c1e1503e
Bennyisrael <112539918+Bennyisrael@users.noreply.github.com>
2024-03-27 19:48:00 +0530

    Time: 5 ms (25.61%), Space: 42.3 MB (27.25%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String str[]=s.split(" ");
+        String st="";
+        for(int i=0;i<k;i++){
+            st+=str[i]+" ";
+        }
+        return st.trim();
+    }
+}
