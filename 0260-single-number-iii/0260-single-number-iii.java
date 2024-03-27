decd19dc3b3161b3eccd55b5c726001569f3b60d
Bennyisrael <112539918+Bennyisrael@users.noreply.github.com>
2024-03-27 19:08:25 +0530

    Time: 6 ms (33.19%), Space: 45.6 MB (59.19%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -4,13 +4,13 @@ class Solution {
         for(int i : nums){
             mp.put(i,mp.getOrDefault(i,0)+1);
         }
-        int c = 0;
-        for (int value : mp.values()) {
-            if (value == 1) {
-                c++;
-            }
-        }
-        int arr[]=new int [c];
+        // int c = 0;
+        // for (int value : mp.values()) {
+        //     if (value == 1) {
+        //         c++;
+        //     }
+        // }
+        int arr[]=new int [2];
         int i=0;
         for(Map.Entry<Integer,Integer> ent : mp.entrySet()){
             if(ent.getValue()==1){
