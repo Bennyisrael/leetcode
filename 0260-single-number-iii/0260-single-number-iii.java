935418c51494aefe44b0d9a8c540dfe091a5e42e
Bennyisrael <112539918+Bennyisrael@users.noreply.github.com>
2024-03-27 19:07:07 +0530

    Time: 7 ms (20.58%), Space: 45.9 MB (15.33%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        Map<Integer,Integer> mp = new HashMap<>();
+        for(int i : nums){
+            mp.put(i,mp.getOrDefault(i,0)+1);
+        }
+        int c = 0;
+        for (int value : mp.values()) {
+            if (value == 1) {
+                c++;
+            }
+        }
+        int arr[]=new int [c];
+        int i=0;
+        for(Map.Entry<Integer,Integer> ent : mp.entrySet()){
+            if(ent.getValue()==1){
+                arr[i++]=ent.getKey();
+            }
+        }
+        return arr;
+    }
+}
