71ee12054220c360c5dd815f7008964f98efb298
Bennyisrael <112539918+Bennyisrael@users.noreply.github.com>
2024-07-22 21:39:48 +0530

    Time: 23 ms (28.48%), Space: 44.9 MB (91.7%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,21 +1,17 @@
 class Solution {
-    public void swap(int[] arr, int index1, int index2) {
-        int temp = arr[index1];
-        arr[index1] = arr[index2];
-        arr[index2] = temp;
-    }
-
-    public void swap(String[] arrr, int index1, int index2) {
-        String temp = arrr[index1];
-        arrr[index1] = arrr[index2];
-        arrr[index2] = temp;
-    }
-
     public String[] sortPeople(String[] names, int[] heights) {
-        for (int i = 0; i < heights.length-1; i++) {
-            if (heights[i + 1] > heights[i]) {
-                swap(heights, i + 1, i);
-                swap(names, i + 1, i);
+        //int lar=heights[0];
+        for(int i=0;i<heights.length;i++){
+            for(int f=i+1;f<names.length;f++){
+                if(heights[i]<heights[f])
+                {
+                int lar=heights[i];
+                String ch=names[i];
+                heights[i]=heights[f];
+                names[i]=names[f];
+                names[f]=ch;
+                heights[f]=lar;
+                }
             }
         }
         return names;
